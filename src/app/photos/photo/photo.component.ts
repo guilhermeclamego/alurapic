920923b32b1957bbdd66ce54b247c45a7eb1bd81
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-20 18:06:24 -0300

    Criado component photos

---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+    //Input, para o app.component.html, ficar dinamico, conforme imsg e url passados lá
+    @Input() description = '';
+    @Input() url = '';
+}
