52bdb29fea6288496a43bee580fad80e086d1d87
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-20 18:29:08 -0300

    Criaçao de lista de fotos, utilizaçao do NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   photos = [
     {
       url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
@@ -15,6 +15,6 @@ export class AppComponent {
     {
       url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
       description: 'Leoa'
-    }
+    }                                                                                           
   ];
 }
