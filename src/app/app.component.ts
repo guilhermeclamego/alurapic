53a9cbb705974edae70b8f2440d71c2aca7ecbb4
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-21 16:21:28 -0300

    Criaçao da interface photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { PhotoService } from './photos/photo/photo.service';
 })
 export class AppComponent {
 
-  photos: Object[] = [];
+  photos: any[] = [];
 
   //Injeção de dependência http, carregado o modulo no app.module
   constructor(photoService: PhotoService){
       photoService.listFromUser('flavio')
                   .subscribe( photos => {
+                    console.log(photos[0].description)
                     this.photos = photos                    
                   });
   }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Photo } from "./photo";
 
 const API = 'http://localhost:3000'
 
@@ -9,6 +10,6 @@ export class PhotoService {
     constructor(private http: HttpClient){} 
 //private e o this, para ser possível ouvir o http dentro da função, sem isso, o http só seria ouvido direto da classe
     listFromUser(userName: string){
-        return this.http.get<Object[]>(`${API}/${userName}/photos`);
+        return this.http.get<Photo[]>(`${API}/${userName}/photos`);
     }
 }
