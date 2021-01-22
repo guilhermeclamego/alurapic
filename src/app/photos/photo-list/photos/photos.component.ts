2b9df2e36f7e2f33f03225263dd06015b4bddf93
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-22 17:33:09 -0300

    Adicionado EventDrive keyup para obter digitaçao do campo search

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,16 +7,17 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
   rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
-    this.rows = this.groupColumns(this.photos);
-  }
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.photos)
+        this.rows = this.groupColumns(this.photos);
+}
 
   groupColumns(photos: Photo[]){
     const newRows = [];
