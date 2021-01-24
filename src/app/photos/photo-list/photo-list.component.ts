262cedfe9c9e0d95e3098b16b4648519151070b9
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-24 19:18:46 -0300

    Adicionado Paginaçao na requisiçao http das fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute){ }
 
   ngOnInit(): void {
 
-    //do app.routing.module.ts, para definir dinamicamente o userName
-    //const userName = this.activatedRoute.snapshot.params.userName 
     this.photos = this.activatedRoute.snapshot.data['photos'];
-   
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);   
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
