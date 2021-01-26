929f5e0b2786bf2d830cb5d667dad133376c4471
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-25 22:01:24 -0300

    Adicionado pipe com tap do rxjs para obter acess-token do response

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000'
 @Injectable({
@@ -11,9 +12,13 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   authenticate(userName: string, password: string){
-    //Como o nome da propriedade é o mesmo do parâmetro, em js, pode ser passar apenas o nome da propriedade
-    // { userName, password } = { userName: userName, password: password}
-    return this.http.post(API_URL + '/user/login', { userName, password })
+
+    return this.http
+      .post(API_URL + '/user/login', { userName, password }, {observe: 'response'})
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(authToken);
+      }))
 
   }
 }
