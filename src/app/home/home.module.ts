0a31d4c71017adb01a14349692e021e6971f6f9c
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-26 22:46:37 -0300

    Inicio do Signup

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/directives/vmessage/vmessage.module';
+import { SignUpComponent } from './signup/signup.component';
 
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent
+     ],
     imports: [ 
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule 
