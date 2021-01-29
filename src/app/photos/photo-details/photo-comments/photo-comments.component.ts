71c117cacbb522113cd0c3111e68ff49a84b1436
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-29 17:25:27 -0300

    Post de comentarios

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
 
@@ -25,5 +27,19 @@ export class PhotoCommentsComponent implements OnInit{
         this.commentForm = this.formBuilder.group({
             comment: ['', Validators.maxLength(300)]
         });
+    }
+
+    save(){
+        const comment = this.commentForm.get('comment').value as string;
+
+        this.comments$ = this.photoService
+            .addComment(this.photoId, comment)
+            .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+            .pipe(tap(() => {
+                this.commentForm.reset();
+                alert('Comentário adicionado com sucesso');
+            }))
+
+            
     }
 }
