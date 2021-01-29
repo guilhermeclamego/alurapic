7e673b61210c056c732ba3f9037b7e15a4cfd142
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-29 15:06:01 -0300

    Rotas e Componente para photo details

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
         canActivate: [AuthGuard] 
     },
     { 
-        path: 'p/:photoid', 
+        path: 'p/:photoId', 
         component: PhotoDetailsComponent        
     },
     { 
--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
     templateUrl: './photo-details.component.html',
 })
-export class PhotoDetailsComponent { 
 
+export class PhotoDetailsComponent implements OnInit { 
+
+    photo: Photo;
+
+    constructor(
+        private route: ActivatedRoute,
+        private photoService: PhotoService
+    ) {}
+
+    ngOnInit(): void {
+        const id = this.route.snapshot.params.photoId;
+        
+        this.photoService
+            .findById(id)
+            .subscribe(photo => this.photo = photo);
+
+    }
 }
