5a4eec90a445e6de30ae17f426993ac85961b43d
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-29 12:02:51 -0300

    Click imediato no upload de imagem

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immedate-click.module';
 import { VMessageModule } from 'src/app/shared/directives/vmessage/vmessage.module';
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoFormComponent } from './photo-form.component';
@@ -16,7 +18,8 @@ import { PhotoFormComponent } from './photo-form.component';
         VMessageModule,
         FormsModule,
         RouterModule,
-        PhotoModule
+        PhotoModule,
+        ImmediateClickModule
     ]   
     
 })
