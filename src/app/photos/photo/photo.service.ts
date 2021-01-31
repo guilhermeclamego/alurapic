c40b0fea3eaa11e194f01ef9d0c0cbafa9a7d0af
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-01-31 18:15:45 -0300

    Curtidas de foto

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
+
 import { Photo } from './photo';
 import { PhotoComment } from './photo-comment';
 
@@ -47,5 +50,15 @@ export class PhotoService {
 
     removePhoto(photoId: number) {
         return this.http.delete(API + '/photos/' + photoId);
+    }
+
+    like(photoId: number) {
+        return this.http.post(
+            API + '/photos/' + photoId + '/like', {}, { observe: 'response'}
+        )
+        .pipe(map(res => true))
+        .pipe(catchError(err => {
+            return err.status == '304' ? of(false) : throwError(err);
+        }));
     }
 }
