3fdf55705c22e06c4faedb66d3d4d0bf5f62c113
guilhermeclamego <guilhermecesarl20@gmail.com>
2021-02-01 18:06:58 -0300

    Criacao de menu da aplicacao

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/menu/menu.module';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 
@@ -22,7 +23,8 @@ import { HeaderComponent } from './header/header.component';
         CommonModule,
         RouterModule,
         AlertModule,
-        LoadingModule
+        LoadingModule,
+        MenuModule
     ],
     providers: [
         {
